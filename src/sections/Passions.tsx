f27d483b78de03077b08374bd65805facddaa20b
WildoDev <wildofari99@gmail.com>
2025-04-30 21:58:14 -0300

    Section deleted

--- src/sections/Passions.tsx
+++ src/sections/Passions.tsx
@@ -104,7 +104,6 @@ export default function Passions() {
                     <PassionItem icon="📚" title="Lectura" description="Un viaje sin fronteras a través de las palabras." images={["/images/reading1.jpg", "/images/reading2.jpg", "/images/reading3.jpg", "/images/reading4.jpg"]} />
                     {/* <PassionItem icon="🏋️" title="Gimnasio y Fitness" description="Disciplina, esfuerzo y bienestar en cada repetición." images={["/images/fitness1.jpg", "/images/fitness2.jpg", "/images/fitness3.jpg"]} /> */}
                     {/* <PassionItem icon="🎬" title="Cine y Series" description="Historias que emocionan y transportan." images={["/images/movies1.jpg", "/images/movies2.jpg", "/images/movies3.jpg"]} /> */}
-                    {/* <PassionItem icon="🔧" title="Tecnología" description="Innovación que transforma la realidad." images={["/images/tech1.jpg", "/images/tech2.jpg", "/images/tech3.jpg"]} />
                     <PassionItem icon="🏆" title="Medallas" description="Mis logros y medallas." images={["/images/medal1.jpg", "/images/medal2.jpg", "/images/medal3.jpg", "/images/medal4.jpg"]} /> */}
                 </div>
             </div>
