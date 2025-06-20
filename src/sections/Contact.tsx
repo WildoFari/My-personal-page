21300485f013defba7d3f2757dda0f296398e87e
WildoDev <wildofari99@gmail.com>
2025-06-20 17:14:56 -0300

    Cambios en contacto

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,8 +1,8 @@
-export default function Contact() {
+        export default function Contact() {
     return (
         <section id="contact" className="py-50 bg-gradient-to-br from-gray-900 to-gray-800 text-white">
             <div className="container mx-auto px-4 py-5 md:py-2 text-center">
-                <h2 className="text-5xl font-extrabold mb-1 md:mb-2 text-gray-300 hover:text-white cursor-default bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 ">
+                <h2 className="text-5xl font-extrabold mb-1 md:mb-2 text-gray-300 hover:text-white cursor-default bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500">
                     Contacto
                 </h2>
                 <p className="text-xl mb-2 hover:text-white text-gray-500 cursor-default">
@@ -10,13 +10,11 @@ export default function Contact() {
                 </p>
 
                 <div className="flex flex-col sm:flex-row justify-center items-center gap-6">
-
                     <a
                         href="https://www.linkedin.com/in/wildo-fariña-gómez-3500522b9"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="group relative inline-flex items-center justify-center px-6 py-3 font-medium text-blue-300 rounded-lg shadow-xl transition-all duration-300 hover:ring-2 hover:ring-blue-400 hover:bg-gray-900 hover:bg-blue-900/30"
-                    >
+                        className="group relative inline-flex items-center justify-center px-6 py-3 font-medium text-blue-300 rounded-lg shadow-xl transition-all duration-300 hover:ring-2 hover:ring-blue-400 hover:bg-gray-900 hover:bg-blue-900/30 transform hover:scale-105">
                         <span className="relative">LinkedIn</span>
                     </a>
 
@@ -24,8 +22,7 @@ export default function Contact() {
                         href="https://www.instagram.com/wildodevpy?igsh=MW81M2lqN3IxNmk3Yw%3D%3D&utm-source=qr"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="group relative inline-flex items-center justify-center px-6 py-3 font-medium text-pink-300 rounded-lg shadow-xl transition-all duration-300 hover:ring-2 hover:ring-pink-400 hover:bg-gray-900 hover:bg-pink-900/30"
-                    >
+                        className="group relative inline-flex items-center justify-center px-6 py-3 font-medium text-pink-300 rounded-lg shadow-xl transition-all duration-300 hover:ring-2 hover:ring-pink-400 hover:bg-gray-900 hover:bg-pink-900/30 transform hover:scale-105">
                         <span className="relative">Instagram</span>
                     </a>
 
@@ -33,8 +30,7 @@ export default function Contact() {
                         href="https://github.com/WildoFari"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="group relative inline-flex items-center justify-center px-6 py-3 font-medium text-indigo-300 rounded-lg shadow-xl transition-all duration-300 hover:ring-2 hover:ring-indigo-400 hover:bg-gray-900 hover:bg-indigo-900/30"
-                    >
+                        className="group relative inline-flex items-center justify-center px-6 py-3 font-medium text-indigo-300 rounded-lg shadow-xl transition-all duration-300 hover:ring-2 hover:ring-indigo-400 hover:bg-gray-900 hover:bg-indigo-900/30 transform hover:scale-105">
                         <span className="relative">GitHub</span>
                     </a>
                 </div>
@@ -47,8 +43,7 @@ export default function Contact() {
                     href="https://www.instagram.com/liz_fonseca18?igsh=MTRrNnp4dDludXg0dw=="
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-block mb-6 px-5 py-2 bg-pink-100 text-pink-700 text-xs rounded-full shadow-md hover:bg-pink-200 transition duration-300"
-                >
+                    className="inline-block mb-6 px-5 py-2 bg-pink-100 text-pink-700 text-xs rounded-full shadow-md hover:bg-pink-200 transition duration-300 transform hover:scale-105">
                     💖 Perfil de mi novia
                 </a>
             </div>
